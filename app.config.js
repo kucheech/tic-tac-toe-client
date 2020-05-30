7e2df07fb0711f7d9d0fa260c95f576fdcf1accd
William Ku <kucheech@gmail.com>
2020-05-30 14:10:06 +0800

    * edit app.json for expo build:android -t apk * corrected app.config.js to incorporate app.json

--- app.config.js
+++ app.config.js
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 
-export default {
-  name: 'Tic Tac Toe',
-  version: '0.0.1',
-  extra: {
-    publishKey: process.env.EXPO_PUBLISHKEY,
-    subscribeKey: process.env.EXPO_SUBSCRIBEKEY,
-    url: process.env.EXPO_API_BASE_URL
-  },
+export default ({ config }) => {
+  return Object.assign(config,
+    {
+      name: 'Tic Tac Toe',
+      version: '0.0.1',
+      extra: {
+        publishKey: process.env.EXPO_PUBLISHKEY,
+        subscribeKey: process.env.EXPO_SUBSCRIBEKEY,
+        url: process.env.EXPO_API_BASE_URL
+      },
+    });
 };
